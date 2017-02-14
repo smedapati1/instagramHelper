6b09067265faf184443c8a42d7e86399148bee58
smedapati1 <medapati.soujanya1@gmail.com>
2017-02-14 15:25:45 -0600

    initialcommit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+router.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+router.get('/contact', function(req, res){
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+router.get('/signup', function(req, res){
+  res.render('signup', {
+    title: 'SignUp'
+  });
+});
+
+module.exports = router;
